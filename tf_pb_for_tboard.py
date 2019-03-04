75b66c56c031e913a4b4b562abb1b97838b4f7bb
Jason Kuan <jason9075@gmail.com>
2019-03-04 11:59:20 +0800

    add while_loop operation to pb model, and I confirm that tensorflow lite is not support tflite converter now....

--- tf_pb_for_tboard.py
+++ tf_pb_for_tboard.py
@@ -4,7 +4,7 @@ from tensorflow.python.platform import gfile
 PB_FILE = "output_models/astra_person_detector.pb"
 
 
-def main():
+def load_pb_file():
     graph = tf.Graph()
     with graph.as_default():
         graph_def = tf.GraphDef()
@@ -12,6 +12,27 @@ def main():
             graph_def.ParseFromString(f.read())
         tf.import_graph_def(graph_def, name="pb")
     tf.summary.FileWriter("tensor_board/", graph=graph)
+
+
+def build_graph():
+    graph = tf.Graph()
+    with graph.as_default() as graph:
+        n = 100
+        x = tf.constant(list(range(n)))
+        c = lambda i, x: i < n
+        b = lambda i, x: (tf.Print(i + 1, [i]), tf.Print(x + 1, [i], "x:"))
+        i, out = tf.while_loop(c, b, (0, x))
+        with tf.Session() as sess:
+            print(sess.run(i))  # prints [0] ... [9999]
+            print(sess.run(out).shape)
+
+    tf.summary.FileWriter("tensor_board/", graph=graph)
+    tf.train.write_graph(graph, "output_models/", "while.pb", as_text=False)
+
+
+def main():
+    # load_pb_file()
+    build_graph()
 
 
 if __name__ == '__main__':
