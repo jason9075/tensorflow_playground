25fd951f098ceba6bf4bd37f7e7f5e66f302c6ff
Jason Kuan <jason9075@gmail.com>
2019-03-05 18:48:31 +0800

    add stack example

--- tf_tensor_index.py
+++ tf_tensor_index.py
@@ -9,13 +9,21 @@ def main():
         split = tf.split([0, 1, 2, 3, 4], 5, name="split_op")  # return list of tensor
         sum_value = tf.add(split[3], c, name="sum")
 
-    tf.summary.FileWriter("tensor_board/", graph=graph)
+        x = tf.constant([1, 4])
+        y = tf.constant([2, 5])
+        z = tf.constant([3, 6])
+        tf.stack([x, y, z], axis=0, name="stack_axis0")  # [[1, 4], [2, 5], [3, 6]] (Pack along first dim.)
+        tf.stack([x, y, z], axis=1, name="stack_axis1")  # [[1, 2, 3], [4, 5, 6]]
+
+    # tf.summary.FileWriter("tensor_board/", graph=graph)
 
     tensor_0 = graph.get_tensor_by_name('split_op:0')
     tensor_1 = graph.get_tensor_by_name('split_op:1')
     tensor_2 = graph.get_tensor_by_name('split_op:2')
     tensor_3 = graph.get_tensor_by_name('split_op:3')
     tensor_4 = graph.get_tensor_by_name('split_op:4')
+
+    stack_1 = graph.get_tensor_by_name('stack_axis1:0')
 
     sess = tf.Session(graph=graph)
 
@@ -25,6 +33,7 @@ def main():
     print('by tensor_3: {}, by parameter:{}'.format(sess.run(tensor_3), sess.run(split[3])))
     print('by tensor_4: {}, by parameter:{}'.format(sess.run(tensor_4), sess.run(split[4])))
     print('sum: {}'.format(sess.run(sum_value)))
+    print('stack_1_0:{}'.format(sess.run(stack_1)))
 
     sess.close()
 
