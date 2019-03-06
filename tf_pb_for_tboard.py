5d1784f8f6c9f544fcfd859eaca6b497de1f5c68
Jason Kuan <jason9075@gmail.com>
2019-03-06 16:41:27 +0800

    add support to pbtxt

--- tf_pb_for_tboard.py
+++ tf_pb_for_tboard.py
@@ -1,4 +1,7 @@
+import pathlib
+
 import tensorflow as tf
+from google.protobuf import text_format
 from tensorflow.python.platform import gfile
 
 PB_FILE = "output_models/astra_person_detector.pb"
@@ -9,7 +12,10 @@ def load_pb_file():
     with graph.as_default():
         graph_def = tf.GraphDef()
         with gfile.FastGFile(PB_FILE, 'rb') as f:
-            graph_def.ParseFromString(f.read())
+            if pathlib.Path(PB_FILE).suffix == '.pbtxt':
+                text_format.Parse(f.read(), graph_def)
+            else:
+                graph_def.ParseFromString(f.read())
         tf.import_graph_def(graph_def, name="pb")
     tf.summary.FileWriter("tensor_board/", graph=graph)
 
@@ -31,8 +37,8 @@ def build_graph():
 
 
 def main():
-    # load_pb_file()
-    build_graph()
+    load_pb_file()
+    # build_graph()
 
 
 if __name__ == '__main__':
